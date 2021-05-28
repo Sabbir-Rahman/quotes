32ec5c93a8a50fe6f09fe10c8a0587486637bfc2
Sabbir-Rahman <sabbirrahman-42@iut-dhaka.edu>
2021-05-29 05:44:43 +0600

    posts data retrieve

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Grid, CircularProgress } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 import Post from './Post/Post'
 import useStyles from './styles'
@@ -7,11 +8,18 @@ const Posts = () => {
     const classes = useStyles()
     
     return(
-        <>
-        <h1>Posts</h1>
-        <Post/>
-        <Post/>
-        </>
+        //if post.length is greater than 0 then it will show the grid otherwise cuircular progress
+        !posts.length ? <CircularProgress /> : (
+            
+            //not decond bracket
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post}/>
+                    </Grid>
+                ))}
+            </Grid>
+        )
     );
 }
 
