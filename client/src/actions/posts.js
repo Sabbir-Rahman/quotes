31fe637995a3b3ec201b65c6b95b3069e96c823a
Sabbir-Rahman <sabbirrahman-42@iut-dhaka.edu>
2021-05-29 22:01:32 +0600

    constants

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,3 +1,4 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes'
 import * as api from '../api/index.js'
 
 // Action Creators
@@ -5,7 +6,7 @@ import * as api from '../api/index.js'
 
     try {
         const { data } = await api.fetchPosts()
-        dispatch({ type: 'FETCH_ALL', payload: data})
+        dispatch({ type: FETCH_ALL, payload: data})
     } catch(error) {
         console.log(error.message)
     }   
@@ -17,7 +18,7 @@ export const createPost = (post) => async (dispatch) => {
 
     try {
         const { data } = await api.createPost(post)
-        dispatch({ type: 'CREATE', payload: data})
+        dispatch({ type: CREATE, payload: data})
         
        
     } catch (error) {
@@ -31,7 +32,7 @@ export const updatePost = (id, post) => async (dispatch) => {
     try {
         const { data } = await api.updatePost(id,post)
 
-        dispatch({ type: 'UPDATE', payload: data})
+        dispatch({ type: UPDATE, payload: data})
     } catch (error) {
         console.log(error)
     }
@@ -45,7 +46,7 @@ export const deletePost = (id) => async (dispatch) => {
         //we don't need to care about response
         await api.deletePost(id)
 
-        dispatch({ type: 'DELETE', payload: id})
+        dispatch({ type: DELETE, payload: id})
     } catch (error) {
         console.log(error)
     }
@@ -55,7 +56,7 @@ export const likePost = (id) => async (dispatch) => {
     try {
         const { data } = await api.likePost(id)
 
-        dispatch({ type: 'UPDATE', payload: data})
+        dispatch({ type: UPDATE, payload: data})
     } catch (error) {
         console.log(error)
     }
