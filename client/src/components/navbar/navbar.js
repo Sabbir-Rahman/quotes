6fa101cd1f9176db5c60f73626e44b4e0a9d1dee
Sabbir <sabbirrahman42@iut-dhaka.edu>
2021-06-10 15:49:07 +0600

    jwt expire logic written

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -4,6 +4,7 @@ import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core'
 import useStyles from './styles'
 import memoriesImg from '../../images/memories.png'
 import { useDispatch } from 'react-redux'
+import decode from 'jwt-decode'
 
 const Navbar = () => {
     const classes = useStyles()
@@ -22,7 +23,11 @@ const Navbar = () => {
     useEffect(() => {
         const token = user?.token
 
-        // JWT
+        if(token){
+            const decodedToken = decode(token)
+
+            if(decodedToken.exp * 1000 < new Date().getItem()) logout()
+        }
 
         setuser(JSON.parse(localStorage.getItem('profile')))
     }, [location])
